ebf40eb53fafaef6268336cfb3b0d0f76c915839
constantius <constantiuskos@gmail.con>
2024-11-20 20:43:04 +0400

    выбирается одна цитата из массива цитат. С помощью обьектов и классов  (OOP)

---
+++ src/classes/RandomQuotesApp.js
@@ -0,0 +1,31 @@
+import RandomQuote from './RandomQuote.js';
+
+class RandomQuotesApp {
+  constructor() {
+    this.currenQuote = null;
+    this.quoteAuthorElement = document.getElementById('author');
+    this.quoteTextElement = document.getElementById('quote-text');
+    this.randomQuoteBtn = document.getElementById('random-quote-btn');
+
+    this.init();
+  }
+
+  displayCurrentQuote() {
+    const { text, author } = this.currenQuote;
+
+    this.quoteTextElement.textContent = `"${text}"`;
+    this.quoteAuthorElement.textContent = author;
+  }
+
+  getRandomQuote() {
+    const randomQuote = RandomQuote.getRandomQuote();
+    this.currenQuote = randomQuote;
+    this.displayCurrentQuote();
+  }
+
+  init() {
+    this.randomQuoteBtn.addEventListener('click', () => this.getRandomQuote());
+  }
+}
+
+export default RandomQuotesApp;
---
+++ src/data/quotes.js
@@ -0,0 +1,79 @@
+const quotes = [
+  {
+    id: '76e5b050',
+    text: 'The only way to do great work is to love what you do',
+    author: 'Steve Jobs',
+  },
+  {
+    id: '5750d772',
+    text: 'Innovation distinguishes between a leader and a follower',
+    author: 'Steve Jobs',
+  },
+  {
+    id: 'f5f92da7',
+    text: "Your time is limited, so don't waste it living someone else's life",
+    author: 'Steve Jobs',
+  },
+  {
+    id: '8d6e3c5f',
+    text: 'Success is not final, failure is not fatal: it is the courage to continue that counts',
+    author: 'Winston Churchill',
+  },
+  {
+    id: '5d1a404a',
+    text: 'The best way to predict the future is to invent it',
+    author: 'Alan Kay',
+  },
+  {
+    id: 'ef04a87d',
+    text: "If you're going through hell, keep going",
+    author: 'Winston Churchill',
+  },
+  {
+    id: '50fcb5c5',
+    text: 'The journey of a thousand miles begins with one step',
+    author: 'Lao Tzu',
+  },
+  {
+    id: 'ec9838a5',
+    text: 'Life is what happens when you’re busy making other plans',
+    author: 'John Lennon',
+  },
+  {
+    id: 'd2eb70dc',
+    text: 'The only limit to our realization of tomorrow is our doubts of today',
+    author: 'Franklin D. Roosevelt',
+  },
+  {
+    id: 'f048bd9b',
+    text: 'Do not go where the path may lead, go instead where there is no path and leave a trail',
+    author: 'Ralph Waldo Emerson',
+  },
+  {
+    id: 'd14d09e7',
+    text: 'It is never too late to be what you might have been',
+    author: 'George Eliot',
+  },
+  {
+    id: 'ec7e899a',
+    text: 'Believe you can and you’re halfway there',
+    author: 'Theodore Roosevelt',
+  },
+  {
+    id: '7a4cc08e',
+    text: 'Success usually comes to those who are too busy to be looking for it',
+    author: 'Henry David Thoreau',
+  },
+  {
+    id: '67272de1',
+    text: 'Do what you can, with what you have, where you are',
+    author: 'Theodore Roosevelt',
+  },
+  {
+    id: 'eefc87e6',
+    text: 'The future belongs to those who believe in the beauty of their dreams',
+    author: 'Eleanor Roosevelt',
+  },
+];
+
+export default quotes;
