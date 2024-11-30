a4c0c8086b1edc355e1830903b134fc65f7b4741
constantius <constantiuskos@gmail.con>
2024-11-30 19:08:09 +0400

    установлены зависимости бота

--- my-telegram-bot/bot.js
+++ my-telegram-bot/bot.js
@@ -1,31 +1,45 @@
 const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
 
-// Замените токен вашего бота
 const token = '7355670727:AAGKQR4De9RK7UU0MAf1ug1Wx35WKUlrx4c';
+const app = express();
+const port = process.env.PORT || 3000; // Используйте переменную окружения PORT или стандартный порт 3000
 
-// Создайте экземпляр бота
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token);
 
+// Настройка вебхуков
+const webhookUrl = `https:https://random-quotes-bot.onrender.com`;
+
+bot.setWebHook(webhookUrl);
+
+// Парсинг запросов от Telegram
+app.use(express.json());
+app.post(`/mybot`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+// Обработчик сообщений
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
-  // Создайте клавиатуру с кнопкой для открытия вашего мини-приложения
   const options = {
     reply_markup: {
       inline_keyboard: [
         [
           {
             text: 'Хочу цитатку',
-            web_app: { url: 'https://constantius-kos.github.io/mytestpage/' }, // Используйте web_app вместо url
+            web_app: { url: 'https://constantius-kos.github.io/mytestpage/' },
           },
         ],
       ],
     },
   };
 
-  // Отправьте сообщение с кнопкой
   bot.sendMessage(chatId, 'Хочешь цитатку?', options);
 });
 
-// Запустите бота
-console.log('Бот запущен...');
+// Запуск сервера
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
