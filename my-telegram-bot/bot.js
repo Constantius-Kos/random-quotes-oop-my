99357ff81a757d6d61f0daa74927b1048176430d
constantius <constantiuskos@gmail.con>
2024-11-30 18:06:03 +0400

    бот тест 1

--- my-telegram-bot/bot.js
+++ my-telegram-bot/bot.js
@@ -5,27 +5,42 @@ const token = '7355670727:AAGKQR4De9RK7UU0MAf1ug1Wx35WKUlrx4c';
 
 // Создайте экземпляр бота
 const bot = new TelegramBot(token, { polling: true });
-
-bot.on('message', (msg) => {
+bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
 
-  // Создайте клавиатуру с кнопкой для открытия вашего мини-приложения
-  const options = {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          {
-            text: 'Хочу цитатку',
-            web_app: { url: 'https://constantius-kos.github.io/mytestpage/' }, // Используйте web_app вместо url
-          },
+  // Отправляем сообщение с кнопкой
+  bot.sendMessage(
+    chatId,
+    'Добро пожаловать! Нажмите "Получить цитату", чтобы начать.',
+    {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: 'Получить цитату', callback_data: 'get_quote' }],
         ],
-      ],
-    },
-  };
-
-  // Отправьте сообщение с кнопкой
-  bot.sendMessage(chatId, 'Хочешь цитатку?', options);
+      },
+    }
+  );
 });
+// bot.on('message', (msg) => {
+//   const chatId = msg.chat.id;
+
+//   // Создайте клавиатуру с кнопкой для открытия вашего мини-приложения
+//   const options = {
+//     reply_markup: {
+//       inline_keyboard: [
+//         [
+//           {
+//             text: 'Хочу цитатку',
+//             web_app: { url: 'https://constantius-kos.github.io/mytestpage/' }, // Используйте web_app вместо url
+//           },
+//         ],
+//       ],
+//     },
+//   };
+
+//   // Отправьте сообщение с кнопкой
+//   bot.sendMessage(chatId, 'Хочешь цитатку?', options);
+// });
 
 // Запустите бота
 console.log('Бот запущен...');
